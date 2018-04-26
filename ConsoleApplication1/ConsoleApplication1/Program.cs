b345be9eb08896479c5d65fac5ac80c90b4e695b
m305-42\w401 <a106082028@mail.shu.edu.tw>
2018-04-26 15:54:14 +0800

    1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // 顏色
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // 輸入
+            Console.Write("請輸入身高(公分):");
+            string height = Console.ReadLine();
+            Console.Write("請輸入體重(公斤):");
+            string weight = Console.ReadLine();
+
+            // 變數                  
+            double BMI = double.Parse(weight) / ((double.Parse(height) / 100) * (double.Parse(height) / 100));
+
+            // 計算
+            Console.Write("BMI=" + BMI);
+
+            // 結果
+            if (BMI >= 27)
+            {
+                Console.Write("過重");
+            }
+
+            if (BMI < 18.5)
+            {
+                Console.Write("過輕");
+            }
+
+            if (BMI > 18.5 && BMI < 24)
+            {
+                Console.WriteLine("標準");
+            }
+            // 結束
+            Console.ReadLine();
+        }
+    }
+}
